49dfb85595f45e86ad5ccd876ec01237b5c02225
HoCG <rhwkd5012@naver.com>
2022-12-15 14:22:51 +0900

    customAxios 타입스크립트로 변경

--- src/apis/customAxios.ts
+++ src/apis/customAxios.ts
@@ -4,11 +4,11 @@ import { getCookie, updateCookie } from './cookie';
 
 const customAxios = axios.create({
   baseURL: process.env.REACT_APP_API_URL, // 기본 서버 주소 입력
-  timeout: JSON.parse(process.env.REACT_APP_AXIOS_TIMEOUT),
+  timeout: JSON.parse(process.env.REACT_APP_AXIOS_TIMEOUT as string),
   withCredentials: true,
   headers: {
     "Content-type": "application/json",
-    access_token: getCookie('access_token'),
+    access_token: getCookie(),
   },
 });
 customAxios.interceptors.request.use((config) => {
